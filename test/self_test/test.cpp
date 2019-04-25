2e393be20c2cbf82c085b926a8d2b5bb4b5a8159
Albert <you@example.com>
2019-04-25 11:07:10 +0800

    finish read

--- test/self_test/test.cpp
+++ test/self_test/test.cpp
@@ -15,7 +15,7 @@ struct Event
     uint64_t timestamp;
 };
 
-struct TrackedFeature
+struct Tracked_Feature
 {
   int id = -1;
   double x = -1.0;
@@ -23,15 +23,64 @@ struct TrackedFeature
 //   double ts = -1;
 };
 
-istream &read_event(istream &is, Event &new_event)
+istream &read_frame(istream &fin, vector<Tracked_Feature> &feature_list, double &timestamp)
 {
-    double tmp_timestamp;
-    is >> tmp_timestamp >> new_event.x >> new_event.y >> new_event.polarity;
-    new_event.timestamp = uint64_t (tmp_timestamp*1e9);
-    // new_event.x = new_event.x % 128;
-    // new_event.y = new_event.y % 128;
-    // cout<<new_event.timestamp<<","<<new_event.x<<","<<new_event.y<<","<<new_event.polarity<<endl;
-    return is;
+    Tracked_Feature temp_Feature;
+    string line;
+    cout<<"Start reading frame (three lines)"<<endl;
+
+    for(int line_counter=0;line_counter<3;line_counter++)
+    {
+        getline(fin, line);
+        istringstream iss(line);
+        if(line_counter%3 == 0)//id row
+        {
+            iss>>timestamp;
+            while(iss>>temp_Feature.id)
+            {
+                // iss>>temp_Feature.id;
+                feature_list.push_back(temp_Feature);
+            }
+        }
+        else if(line_counter%3 == 1)//x row
+        {
+            double temp = -1;
+            double temp_x = -1;
+            int position = 0;
+            iss>>temp;
+            if(temp!=timestamp)
+            {
+                cout<<"ts="<<temp<<", timestamp is wrong!"<<endl;
+                // return 0;
+            }
+            while(iss>>temp_x)
+            {
+                // iss>>temp_x;
+                feature_list.at(position).x = temp_x;
+                position++;
+            }
+        }
+        else if(line_counter%3 == 2)//y row
+        {
+            double temp = -1;
+            double temp_y = -1;
+            int position = 0;
+            iss>>temp;
+            if(temp!=timestamp)
+            {
+                cout<<"ts="<<temp<<", timestamp is wrong!"<<endl;
+                // return 0;
+            }
+            while(iss>>temp_y)
+            {
+                // iss>>temp_y;
+                feature_list.at(position).y = temp_y;
+                position++;
+            }
+        }
+    }
+    
+    return fin;
 }
 
 /*
@@ -41,69 +90,20 @@ istream &read_event(istream &is, Event &new_event)
  */
 int main(int argc, char const *argv[])
 {
-    ifstream fin("/home/albert/workSpace/data/new_result.txt");
-    vector<TrackedFeature> feature_list;
-    TrackedFeature temp_Feature;
+    ifstream fin("/home/albert/workSpace/data/output_result_day_long2.txt");
+    vector<Tracked_Feature> feature_list;
     double timestamp = -1.0;
-    string line;
-    int line_counter = 0;
-    cout<<"Start reading file"<<endl;
-    while(getline(fin, line))
+    while(read_frame(fin, feature_list, timestamp))
     {
-        line_counter++;
-        istringstream iss(line);
-        if(line_counter%3 == 1)//id row
-        {
-            iss>>timestamp;
-            while(iss)
-            {
-                iss>>temp_Feature.id;
-                feature_list.push_back(temp_Feature);
-            }
-        }
-        else if(line_counter%3 == 2)//x row
-        {
-            double temp = -1;
-            double temp_x = -1;
-            int position = 1;
-            iss>>temp;
-            if(temp!=timestamp)
-            {
-                cout<<"ts="<<temp<<", timestamp is wrong!"<<endl;
-                return 0;
-            }
-            while(iss)
-            {
-                iss>>temp_x;
-                feature_list.at(position).x = temp_x;
-                position++;
-            }
-        }
-        else //y row
-        {
-            double temp = -1;
-            double temp_y = -1;
-            int position = 1;
-            iss>>temp;
-            if(temp!=timestamp)
-            {
-                cout<<"ts="<<temp<<", timestamp is wrong!"<<endl;
-                return 0;
-            }
-            while(iss)
-            {
-                iss>>temp_y;
-                feature_list.at(position).y = temp_y;
-                position++;
-            }
-        }
-        cout<<"\nResult "<<line_counter<<":\n";
+        cout<<"\nResult "<<":\n";
         for(auto ft:feature_list)
         {
             cout<<timestamp<<"  "<<ft.id<<"  "<<ft.x<<"  "<<ft.y<<endl;
         }
-
+        timestamp = -1.0;
+        vector<Tracked_Feature>().swap(feature_list);
     }
+    
     return 0;
 }
 
