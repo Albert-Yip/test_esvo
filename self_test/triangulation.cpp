8f7ebc842d9e8325ad78cfdbf000627719af028a
Albert <you@example.com>
2019-04-26 23:25:09 +0800

    add tri-depth estimate

--- self_test/triangulation.cpp
+++ self_test/triangulation.cpp
@@ -7,7 +7,7 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-// #include "extra.h" // use this if in OpenCV2 
+// #include "extra.h" // used in opencv2 
 using namespace std;
 using namespace cv;
 
@@ -22,14 +22,23 @@ struct TrackedFeature
 //read three lines from the output_result, i.e. one spatiotemporal window - or one frame.
 istream &read_frame(istream &fin, vector<TrackedFeature> &feature_list, double &timestamp);
 
+void feature_match (vector<TrackedFeature> &feature_list_1, 
+                    vector<TrackedFeature> &feature_list_2,
+                    vector<Point2f> &points1,
+                    vector<Point2f> &points2);
 
-void pose_estimation_2d2d (
-    vector<TrackedFeature> &feature_list_1,
-    vector<TrackedFeature> &feature_list_2,
-    Mat& R, Mat& t );
+void pose_estimation_2d2d (vector<Point2f> &points1,
+                           vector<Point2f> &points2,
+                           Mat& R, Mat& t );
+
+void triangulation ( 
+    const vector<Point2f> &points1,
+    const vector<Point2f> &points2, 
+    const Mat& R, const Mat& t, 
+    vector< Point3d >& points );
 
 // 像素坐标转相机归一化坐标
-Point2d pixel2cam ( const Point2d& p, const Mat& K );
+Point2f pixel2cam( const Point2d& p, const Mat& K );
 
 int main ( int argc, char** argv )
 {
@@ -38,52 +47,47 @@ int main ( int argc, char** argv )
     double timestamp_1 = -1.0, timestamp_2 = -1.0;
     read_frame(fin, feature_list_1, timestamp_1);
     read_frame(fin, feature_list_2, timestamp_2);
+    vector<Point2f> points1;
+    vector<Point2f> points2;
+    feature_match(feature_list_1,feature_list_2,points1,points2);
     //-- 估计两张图像间运动
     Mat R,t;
-    pose_estimation_2d2d ( feature_list_1, feature_list_2, R, t );
-
-    //-- 验证E=t^R*scale
-    Mat t_x = ( Mat_<double> ( 3,3 ) <<
-                0,                      -t.at<double> ( 2,0 ),     t.at<double> ( 1,0 ),
-                t.at<double> ( 2,0 ),      0,                      -t.at<double> ( 0,0 ),
-                -t.at<double> ( 1,0 ),     t.at<double> ( 0,0 ),      0 );
-
-    cout<<"t^R="<<endl<<t_x*R<<endl;
-
-    //-- 验证对极约束
-    // Mat K = ( Mat_<double> ( 3,3 ) << 194.8, 0, 170.2, 0, 194.8, 127.0, 0, 0, 1 );
-    // for ( DMatch m: matches )
-    // {
-    //     Point2d pt1 = pixel2cam ( keypoints_1[ m.queryIdx ].pt, K );
-    //     Mat y1 = ( Mat_<double> ( 3,1 ) << pt1.x, pt1.y, 1 );
-    //     Point2d pt2 = pixel2cam ( keypoints_2[ m.trainIdx ].pt, K );
-    //     Mat y2 = ( Mat_<double> ( 3,1 ) << pt2.x, pt2.y, 1 );
-    //     Mat d = y2.t() * t_x * R * y1;
-    //     cout << "epipolar constraint = " << d << endl;
-    // }
+    pose_estimation_2d2d ( points1, points2, R, t );
+    //-- 三角化
+    vector<Point3d> points;
+    triangulation( points1, points2, R, t, points );
+    
+    //-- 验证三角化点与特征点的重投影关系
+    Mat K = ( Mat_<double> ( 3,3 ) << 194.8, 0, 170.2, 0, 194.8, 127.0, 0, 0, 1);
+    for ( int i=0; i<points1.size(); i++ )
+    {
+        Point2d pt1_cam = pixel2cam( points1[i], K );
+        Point2d pt1_cam_3d(
+            points[i].x/points[i].z, 
+            points[i].y/points[i].z 
+        );
+        
+        cout<<"point in the first camera frame: "<<pt1_cam<<endl;
+        cout<<"point projected from 3D "<<pt1_cam_3d<<", d="<<points[i].z<<endl;
+        
+        // 第二个图
+        Point2f pt2_cam = pixel2cam( points2[i], K );
+        Mat pt2_trans = R*( Mat_<double>(3,1) << points[i].x, points[i].y, points[i].z ) + t;
+        pt2_trans /= pt2_trans.at<double>(2,0);
+        cout<<"point in the second camera frame: "<<pt2_cam<<endl;
+        cout<<"point reprojected from second frame: "<<pt2_trans.t()<<endl;
+        cout<<endl;
+    }
+    
     return 0;
 }
 
-Point2d pixel2cam ( const Point2d& p, const Mat& K )
+void feature_match (vector<TrackedFeature> &feature_list_1, 
+                    vector<TrackedFeature> &feature_list_2,
+                    vector<Point2f> &points1,
+                    vector<Point2f> &points2)
 {
-    return Point2d
-           (
-               ( p.x - K.at<double> ( 0,2 ) ) / K.at<double> ( 0,0 ),
-               ( p.y - K.at<double> ( 1,2 ) ) / K.at<double> ( 1,1 )
-           );
-}
-
-
-void pose_estimation_2d2d (vector<TrackedFeature> &feature_list_1, 
-                           vector<TrackedFeature> &feature_list_2, 
-                           Mat& R,  Mat& t )
-{
-    // 相机内参,TUM Freiburg2
-    Mat K = ( Mat_<double> ( 3,3 ) << 194.8, 0, 170.2, 0, 194.8, 127.0, 0, 0, 1 );
-
     //-- 把匹配点转换为vector<Point2f>的形式
-    vector<Point2f> points1;
-    vector<Point2f> points2;
     Point2f temp_point;
     int match_counter = 0;
     
@@ -103,6 +107,14 @@ void pose_estimation_2d2d (vector<TrackedFeature> &feature_list_1,
             }
         }
     }
+}
+
+void pose_estimation_2d2d (vector<Point2f> &points1,
+                           vector<Point2f> &points2, 
+                           Mat& R,  Mat& t )
+{
+    // 相机内参,TUM Freiburg2
+    Mat K = ( Mat_<double> ( 3,3 ) << 194.8, 0, 170.2, 0, 194.8, 127.0, 0, 0, 1 );
 
     for(int i=0;i<points1.size();i++)
     {
@@ -131,6 +143,57 @@ void pose_estimation_2d2d (vector<TrackedFeature> &feature_list_1,
     cout<<"R is "<<endl<<R<<endl;
     cout<<"t is "<<endl<<t<<endl;
     
+}
+
+void triangulation ( 
+    const vector<Point2f> &points1,
+    const vector<Point2f> &points2, 
+    const Mat& R, const Mat& t, 
+    vector< Point3d >& points )
+{
+    Mat T1 = (Mat_<float> (3,4) <<
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0);
+    Mat T2 = (Mat_<float> (3,4) <<
+        R.at<double>(0,0), R.at<double>(0,1), R.at<double>(0,2), t.at<double>(0,0),
+        R.at<double>(1,0), R.at<double>(1,1), R.at<double>(1,2), t.at<double>(1,0),
+        R.at<double>(2,0), R.at<double>(2,1), R.at<double>(2,2), t.at<double>(2,0)
+    );
+    
+    Mat K = ( Mat_<double> ( 3,3 ) << 194.8, 0, 170.2, 0, 194.8, 127.0, 0, 0, 1);
+    vector<Point2f> pts_1, pts_2;
+    for (int  i=0;i<points1.size();i++ )
+    {
+        // 将像素坐标转换至相机坐标
+        pts_1.push_back ( pixel2cam( points1[i], K) );
+        pts_2.push_back ( pixel2cam( points2[i], K) );
+    }
+    
+    Mat pts_4d;
+    cv::triangulatePoints( T1, T2, pts_1, pts_2, pts_4d );
+    
+    // 转换成非齐次坐标
+    for ( int i=0; i<pts_4d.cols; i++ )
+    {
+        Mat x = pts_4d.col(i);
+        x /= x.at<float>(3,0); // 归一化
+        Point3d p (
+            x.at<float>(0,0), 
+            x.at<float>(1,0), 
+            x.at<float>(2,0) 
+        );
+        points.push_back( p );
+    }
+}
+
+Point2f pixel2cam ( const Point2d& p, const Mat& K )
+{
+    return Point2f
+    (
+        ( p.x - K.at<double>(0,2) ) / K.at<double>(0,0), 
+        ( p.y - K.at<double>(1,2) ) / K.at<double>(1,1) 
+    );
 }
 
 //read three lines from the output_result, i.e. one spatiotemporal window - or one frame.
@@ -188,6 +251,5 @@ istream &read_frame(istream &fin, vector<TrackedFeature> &feature_list, double &
             }
         }
     }
-    
     return fin;
 }
